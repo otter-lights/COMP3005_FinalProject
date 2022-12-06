adf523d8c94c74daacf6d4be7f4b9cf4e105a7fb
otter-lights <razz.routly@carleton.ca>
2022-12-05 22:41:32 -0500

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS Publisher_Info(
   address   VARCHAR(255),
   email     VARCHAR(255),
   phone_num CHAR(10),
-  bank_num  CHAR(10)
+  bank_num  CHAR(10) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS BOOK(
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS BOOK(
   price     DECIMAL(10,2) NOT NULL,
   stock     INT NOT NULL,
   sales     INT NOT NULL,
-  pid       INT,
+  pid       INT NOT NULL,
   FOREIGN KEY(pid) REFERENCES Publisher_Info(pid),
   pub_cut   DECIMAl(10,2) NOT NULL
 );
@@ -32,6 +32,40 @@ CREATE TABLE IF NOT EXISTS AUTHORS(
   FOREIGN KEY (ISBN) REFERENCES BOOK(ISBN),
   author_name   VARCHAR(255),
   PRIMARY KEY(ISBN, author_name)
+);
+
+CREATE TABLE IF NOT EXISTS CREDIT_CARD(
+  cid         INT PRIMARY KEY,
+  card_num    CHAR(10) NOT NULL,
+  name        VARCHAR(255) NOT NULL,
+  postal_code CHAR(6) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS USER_TABLE(
+  uid           INT PRIMARY KEY,
+  email         VARCHAR(255) NOT NULL,
+  password      VARCHAR(20) NOT NULL,
+  address       VARCHAR(255),
+  default_card  INT,
+  FOREIGN KEY (default_card) REFERENCES CREDIT_CARD(cid),
+  is_admin      BIT
+);
+
+CREATE TABLE IF NOT EXISTS ORDER_TABLE(
+  onum            INT PRIMARY KEY,
+  tracking_num    INT,
+  uid             INT,
+  FOREIGN KEY (uid) REFERENCES USER_TABLE(uid),
+  payment         INT,
+  FOREIGN KEY (payment) REFERENCES CREDIT_CARD(cid)
+);
+
+CREATE TABLE IF NOT EXISTS ORDER_CONTAINS(
+  onum    INT,
+  FOREIGN KEY (onum) REFERENCES ORDER_TABLE(onum),
+  ISBN    CHAR(13),
+  FOREIGN KEY (ISBN) REFERENCES BOOK(ISBN),
+  PRIMARY KEY(onum, ISBN)
 );
 
 /*
@@ -79,4 +113,4 @@ CREATE TABLE IF NOT EXISTS DEPENDENT(
   bdate           DATE,
   relationship    VARCHAR(8),
   PRIMARY KEY(essn, dependent_name)
-);
+);*/
